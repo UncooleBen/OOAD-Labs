8841ec8943e30c9f3068e3cefdce4f6292b76242
uncooleben <pengjuntao2016@foxmail.com>
2019-09-24 15:54:19 +0800

    Build with JDK 1.8

--- lab01-ant/src/com/uncooleben/OOAD/lab01/main/Main.java
+++ lab01-ant/src/com/uncooleben/OOAD/lab01/main/Main.java
@@ -1,8 +1,5 @@
 package com.uncooleben.OOAD.lab01.main;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.swing.JFrame;
 
 import com.uncooleben.OOAD.lab01.gui.AntGameFrame;
@@ -12,16 +9,6 @@ public class Main {
 	private static AntGameFrame frame;
 
 	public static void main(String[] args) {
-		int a[] = new int[] { 0, 1, 2, 3, 4 };
-		List<Integer> list = new ArrayList<Integer>();
-		for (int i : a) {
-			list.add(i);
-		}
-		int sum = 0;
-		for (int i : list) {
-			sum += i;
-		}
-		System.out.print(sum);
 		long start = System.currentTimeMillis();
 		frame = new AntGameFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
