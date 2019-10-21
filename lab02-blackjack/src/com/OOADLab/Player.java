5a1e3df64c165f4d81d4da58542eca3a84d4012e
uncooleben <pengjuntao2016@foxmail.com>
2019-10-21 12:52:44 +0800

    Updated comments

--- lab02-blackjack/src/com/OOADLab/Player.java
+++ lab02-blackjack/src/com/OOADLab/Player.java
@@ -6,61 +6,89 @@ import java.util.List;
 /**
  * This class is used in lab02-blackjack as a part of OOAD-Labs.
  *
- * <p>This class represents a player during the game.
+ * <p>
+ * This class represents a player during the game.
  *
  * @author Shangzhen Li
  */
 public class Player {
-  private List<Hand> hands;
-  private int chipBalance;
-  private int currentBet;
+	private List<Hand> hands;
+	private int chipBalance;
+	private int currentBet;
 
-  Player() {
-    hands = new ArrayList<>();
-    addHand(); // Player has 1 hand by default.
-    chipBalance = 0;
-    currentBet = 0;
-  }
+	/**
+	 * Constructor
+	 */
+	Player() {
+		hands = new ArrayList<>();
+		addHand(); // Player has 1 hand by default.
+		chipBalance = 0;
+		currentBet = 0;
+	}
 
-  public void addHand() {
-    hands.add(new Hand());
-  }
+	/**
+	 * Create and add a new hand to player
+	 */
+	public void addHand() {
+		hands.add(new Hand());
+	}
 
-  protected void addCard(Card c, int handNum) {
-    hands.get(handNum).addCard(c);
-  }
+	/**
+	 * Add a card to a hand of player specified by number
+	 * 
+	 * @param c       A Card object to add
+	 * @param handNum The number of hand in hands list
+	 */
+	protected void addCard(Card c, int handNum) {
+		hands.get(handNum).addCard(c);
+	}
 
-  public void addChipBalance(int amount) {
-    chipBalance += amount;
-  }
+	/**
+	 * Add an amount of chip to the player
+	 * 
+	 * @param amount The amount of chip to add
+	 */
+	public void addChipBalance(int amount) {
+		chipBalance += amount;
+	}
 
-  public void applyReward(int reward) {
-    chipBalance += reward;
-  }
+	/**
+	 * Apply the game reward (positive or negative) to player's chipBalance
+	 * 
+	 * @param reward The game reward (positive or negative)
+	 */
+	public void applyReward(int reward) {
+		chipBalance += reward;
+	}
 
-  public int getHandValue(int handNum) {
-    return hands.get(handNum).calculateValue();
-  }
+	/**
+	 * Getter for handValue
+	 * 
+	 * @return The hand value
+	 */
+	public int getHandValue(int handNum) {
+		return hands.get(handNum).calculateValue();
+	}
 
-  public void payBet(int amount) {
-    currentBet = amount;
-    chipBalance -= amount;
-  }
+	public void payBet(int amount) {
+		currentBet = amount;
+		chipBalance -= amount;
+	}
 
-  public int getChipBalance() {
-    return chipBalance;
-  }
+	public int getChipBalance() {
+		return chipBalance;
+	}
 
-  public int getCurrentBet() {
-    return currentBet;
-  }
+	public int getCurrentBet() {
+		return currentBet;
+	}
 
-  public Hand getHand(int handNum) {
-    return hands.get(handNum);
-  }
+	public Hand getHand(int handNum) {
+		return hands.get(handNum);
+	}
 
-  public void resetHand() {
-    hands.clear();
-    hands.add(new Hand());
-  }
+	public void resetHand() {
+		hands.clear();
+		hands.add(new Hand());
+	}
 }
